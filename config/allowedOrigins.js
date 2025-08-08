b04fed3a7e5ab52bba690ba3b38986f36ac7ddb1
Non-dual-being <kevindeschepper@hotmail.com>
2025-08-08 23:27:15 +0200

    deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:3500',
-    'http://localhost:5173',
-    'htpp://www.dandrepairshop.com',
-    'https://dandrepairshop.com'
+    'https://technotesmernstack.onrender.com'
 ]
 
 module.exports = allowedOrigins;
